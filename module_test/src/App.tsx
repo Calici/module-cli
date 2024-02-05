383047f9b6f3735089fce2650db1acf5e62b0d48
naxhax <nathan@calici.co>
2024-02-05 18:06:55 +0900

    cleanup: removed unused files

--- module_test/src/App.tsx
+++ module_test/src/App.tsx
@@ -1,21 +1,12 @@
-import { UNIVERSAL_TOKEN } from "./API/base"
-import { useParams } from "react-router-dom"
-import ParamsContainer from "./params-container"
 import CaliciSocket from "./socket"
 
-
 const App = () => {
-  console.log(process.env.REACT_APP_WS_ENDPOINT)
-  const params = useParams()
-  const moduleId = params.moduleId
-  const useNew = params.useNew
-  const jwt = UNIVERSAL_TOKEN.jwtToken
-  const url = useNew !== undefined ?
-    `${process.env.REACT_APP_WS_ENDPOINT}new_process/`: undefined
+  const moduleId = "1"
+  const jwt = "1"
+  const url = `${process.env.REACT_APP_WS_ENDPOINT}`
   if (!moduleId) return <></>
   return (
     <div className = 'module-run box-simulation'>
-      <ParamsContainer moduleId = {parseInt(moduleId)} className = 'module-run' />
       <div className = 'module-socket'>
         <CaliciSocket
           moduleId = {parseInt(moduleId)} jwt = {jwt} url = {url}
